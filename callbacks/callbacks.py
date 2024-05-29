99cfeaa3aab40af4802eadebd4fa5bebada745bc
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-05-29 09:54:27 +0900

    create callback func

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 WEIGHT_PATH = os.getenv('WEIGHT_PATH')
-FILEPATH = WEIGHT_PATH + '.{epoch:02d}-{val_loss:.10f}.weights.h5'
+FILEPATH = WEIGHT_PATH + '\weight.{epoch:02d}-{val_loss:.10f}.weights.h5'
 # validation loss가 향상되는 모델만 저장.
 mcp_cb = ModelCheckpoint(filepath= FILEPATH, monitor='val_loss',
                          save_best_only=True, save_weights_only=True, mode='min', verbose=1)
